ebfb337a537ee3e4157bc9b0427dd919fe36dbad
Оксана Ткаченко <oksana@otbook.local>
2018-06-15 10:54:10 +0300

    added npm request and requesr Weather API

--- index.js
+++ index.js
@@ -5,7 +5,18 @@ const bot = new TelegramBot(token, {
   polling: true
 });
 
-var CronJob = require('cron').CronJob;
+const PLACES = [
+  {name: 'Netanya', id: '294071'}
+];
+const url = 'https://api.openweathermap.org/data/2.5/weather?id=294071&appid=b1b35bba8b434a28a0be2a3e1071ae5b&units=metric';
+
+const request = require('request');
+request(url, function (error, response, body) {
+  console.log('statusCode:', response && response.statusCode);
+  console.log('body:', body);
+});
+
+const CronJob = require('cron').CronJob;
 new CronJob('* * * * * *', function() {
   console.log('You will see this message every second');
 }, null, true, 'America/Los_Angeles');
@@ -15,3 +26,4 @@ bot.on('message', (msg) => {
   console.log(msg);
   bot.sendMessage(chatId, msg.text);
 });
+
